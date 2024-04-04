0788d17581a527a2a678aa26dc25105647e5085d
Alifioditya <alifioditya@gmail.com>
2024-04-05 00:48:17 +0700

    fix: apply transformation before combining shapes

--- src/lib/convexHull.ts
+++ src/lib/convexHull.ts
@@ -1,4 +1,5 @@
 import { Point, Shape } from "@/types/Shapes";
+import { transformLine, transformPolygon, transformRectangle, transformSquare } from "./transform";
 
 function cross(o: Point, a: Point, b: Point): number {
     return (a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x);
@@ -11,19 +12,23 @@ export function convexHull(shapes: Shape[]): Point[] {
     // Extract all points from shapes
     shapes.forEach(shape => {
         if (shape.type === "line") {
+            shape = transformLine(shape);
             points.push(shape.start);
             points.push(shape.end);
         } else if (shape.type === "square") {
+            shape = transformSquare(shape);
             points.push({ ...shape.start, color: shape.vertexColors.bl });
             points.push({ ...shape.start, x: shape.start.x + shape.sideLength, color: shape.vertexColors.br });
             points.push({ ...shape.start, y: shape.start.y + shape.sideLength, color: shape.vertexColors.tl });
             points.push({ ...shape.start, x: shape.start.x + shape.sideLength, y: shape.start.y + shape.sideLength, color: shape.vertexColors.tr });
         } else if (shape.type === "rectangle") {
+            shape = transformRectangle(shape);
             points.push({ ...shape.start, color: shape.vertexColors.bl });
             points.push({ ...shape.start, x: shape.start.x + shape.width, color: shape.vertexColors.br });
             points.push({ ...shape.start, y: shape.start.y + shape.height, color: shape.vertexColors.tl });
             points.push({ ...shape.start, x: shape.start.x + shape.width, y: shape.start.y + shape.height, color: shape.vertexColors.tr });
         } else if (shape.type === "polygon") {
+            shape = transformPolygon(shape);
             points = points.concat(shape.vertices);
         }
     });
