c807e39662b98f3d3d53a696a62136e78c2cca04
Alifioditya <alifioditya@gmail.com>
2024-04-05 13:29:05 +0700

    fix: attribute issues

--- src/layouts/Canvas.tsx
+++ src/layouts/Canvas.tsx
@@ -257,16 +257,14 @@ export default function Canvas({ shapePanel, shapes, setShapes }: CanvasProps): 
               type: "square",
               start: { type: 'point', x: mousePos.x - squareSize / 2, y: mousePos.y - squareSize / 2, z: 0, color: { r: 255, g: 255, b: 255, a: 1 } },
               sideLength: squareSize,
-              color: { r: Math.floor(Math.random() * 255), g: Math.floor(Math.random() * 255), b: Math.floor(Math.random() * 255), a: 1 },
               effect: { dx: 0, dy: 0, rotate: 0, scale: 1 },
               final: [
                 { type: 'point', x: 0, y: 0, z: 0, color: { r: 255, g: 255, b: 255, a: 1 } },
                 { type: 'point', x: squareSize, y: 0, z: 0, color: { r: 255, g: 255, b: 255, a: 1 } },
                 { type: 'point', x: 0, y: squareSize, z: 0, color: { r: 255, g: 255, b: 255, a: 1 } },
                 { type: 'point', x: squareSize, y: squareSize, z: 0, color: { r: 255, g: 255, b: 255, a: 1 } }
-            ]
+              ],
               vertexColors: { tl: color, tr: color, bl: color, br: color },
-              effect: { dx: 0, dy: 0, rotate: 0, scale: 1 },
             };
             debouncedSetShapes([...shapes, newSquare]);
           } else if (shapePanel === "rectangle") {
